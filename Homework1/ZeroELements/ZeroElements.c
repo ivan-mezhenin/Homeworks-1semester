a3709df0f43fdb36575ec10b52128d4ccd87b20e
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-09-28 17:46:25 +0300

    first commit

---
+++ Homework1/ZeroELements/ZeroElements.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int zeroCounter(int massive[])
+{
+    int counter = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (massive[i] == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int massive[10];
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &massive[i]);
+
+    }
+    printf("Колличество нулей в массиве:%d", zeroCounter(massive));
+}
+
---
+++ Homework1/swap/swap.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b;
+    printf("Введите 2 числа\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("%d %d", a, b);
+}
+
