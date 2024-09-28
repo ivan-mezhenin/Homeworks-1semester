331b63f28cb6201657a143af90535bc5dcb92ed6
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-09-29 01:46:37 +0300

    new homework

---
+++ Homework3/BubbleSortAndCountingSorting/BubbleSortAndCountingSorting.c
@@ -0,0 +1,133 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define ARRAY_LENGTH 100
+
+void fillingArrayOfRandowNumbers(int *array, int length) {
+    for (int i = 0; i < length; ++i) {
+        array[i] = rand() % 100 + 1;
+    }
+}
+
+void arrayOutput(int array[], int length) {
+    for (int i = 0; i < length; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void swapElements(int* left, int* right) {
+    if (*left != *right) {
+        *left ^= *right;
+        *right ^= *left;
+        *left ^= *right;
+
+    }
+}
+void bubbleSort(int* array, int length) {
+
+    for (int i = 0; i < length - 1; ++i) {
+        for (int j = 0; j < length - 1 - i; ++j) {
+            if (array[j] > array[j + 1]) {
+                swapElements(&array[j], &array[j + 1]);
+            }
+        }
+    }
+}
+
+void countingSort(int* array, int length) {
+    int maxOfarray = 0;
+    for (int i = 0; i < length; ++i) {
+        if (array[i] > maxOfarray) {
+            maxOfarray = array[i];
+        }
+    }
+    int* indexArray = calloc(maxOfarray + 1, sizeof(int));
+
+    for (int i = 0; i < length; ++i) {
+        const int index = array[i];
+        indexArray[index]++;
+    }
+    int counter = 0;
+    for (int i = 0; i <= maxOfarray; ++i) {
+        if (indexArray[i] > 0) {
+            while (indexArray[i] != 0) {
+                array[counter++] = i;
+                indexArray[i]--;
+            }
+        }
+    }
+    free(indexArray);
+}
+
+bool testCorrectCaseCountingSort() {
+    int* unsortedArray = malloc(ARRAY_LENGTH * sizeof(int));
+    fillingArrayOfRandowNumbers(unsortedArray, ARRAY_LENGTH);
+    countingSort(unsortedArray, ARRAY_LENGTH);
+    for (int i = 1; i < ARRAY_LENGTH; ++i) {
+        if (unsortedArray[i - 1] > unsortedArray[i]) {
+            return false;
+        }
+    }
+    free(unsortedArray);
+    return true;
+}
+
+bool testCorrectCaseBubbleSort() {
+    int* unsortedArray = malloc(ARRAY_LENGTH * sizeof(int));
+    fillingArrayOfRandowNumbers(unsortedArray, ARRAY_LENGTH);
+    bubbleSort(unsortedArray, ARRAY_LENGTH);
+    for (int i = 1; i < ARRAY_LENGTH; ++i) {
+        if (unsortedArray[i - 1] > unsortedArray[i]) {
+            return false;
+        }
+    }
+    free(unsortedArray);
+    return true;
+
+}
+
+int main(void) {
+    if (!testCorrectCaseBubbleSort() || !testCorrectCaseCountingSort()) {
+        printf("Tests failed!!\n");
+        return;
+    }
+
+    int length = 0;
+    printf("Inter the length of array: ");
+    scanf("%d", &length);
+    printf("\n");
+    int* unsortedArray = malloc(length * sizeof(int));
+    fillingArrayOfRandowNumbers(unsortedArray, length);
+
+    if (length < 50) {
+        printf("Array Before sorting:\n");
+        arrayOutput(unsortedArray, length);
+    }
+    printf("\n");
+
+    clock_t timeStart = clock();
+    countingSort(unsortedArray, length);
+    clock_t timeCountingSort = clock() - timeStart;
+
+    if (length < 50) {
+        printf("Array Before sorting:\n");
+        arrayOutput(unsortedArray, length);
+    }
+    printf("\n");
+
+    fillingArrayOfRandowNumbers(unsortedArray, length);
+    clock_t timeStartBubbleSort = clock();
+    bubbleSort(unsortedArray, length);
+    clock_t timeBubbleSort = clock() - timeStartBubbleSort;
+
+    printf("Time of bubble sorting: %d\n", timeBubbleSort);
+    printf("Time of counting sorting: %d\n", timeCountingSort);
+
+    free(unsortedArray);
+
+}
---
+++ Homework3/HalfQSort/HalfQSort.c
@@ -0,0 +1,97 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void fillingArrayOfRandowNumbers(int array[], int length) {
+    for (int i = 0; i < length; ++i) {
+        array[i] = rand() % 100 + 1;
+    }
+}
+
+void arrayOutput(int array[], int length) {
+    for (int i = 0; i < length; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void swapElements(int* left, int* right) {
+    if (*left != *right) {
+        *left ^= *right;
+        *right ^= *left;
+        *left ^= *right;
+
+    }
+}
+
+void halfOfQSort(int array[], int length) {
+    const int firstElement = array[0];
+    int leftPointer = 0;
+    int rightPointer = length - 1;
+    while (leftPointer <= rightPointer) {
+        while (array[leftPointer] < firstElement) {
+            leftPointer++;
+        }
+        while (array[rightPointer] >= firstElement) {
+            rightPointer--;
+        }
+        if (leftPointer <= rightPointer) {
+            swapElements(&array[leftPointer], &array[rightPointer]);
+            leftPointer++;
+            rightPointer--;
+        }
+    }
+}
+
+bool testCorrectCase() {
+    int arrayLength = 20;
+    int* unsortedArray = malloc(arrayLength * sizeof(int));
+    fillingArrayOfRandowNumbers(unsortedArray, arrayLength);
+
+    const int firstNumber = unsortedArray[0];
+    int countOfNubersThatAreSmallerThanFirstNumber = 0;
+
+    for (int i = 0; i < arrayLength; ++i) {
+        if (unsortedArray[i] < firstNumber) {
+            countOfNubersThatAreSmallerThanFirstNumber++;
+        }
+    }
+
+    halfOfQSort(unsortedArray, arrayLength);
+
+    for (int i = 0; i < countOfNubersThatAreSmallerThanFirstNumber; ++i) {
+        if (unsortedArray[i] >= firstNumber) {
+            
+            return false;
+        }
+    }
+
+    for (int i = countOfNubersThatAreSmallerThanFirstNumber; i < arrayLength; ++i) {
+        if (unsortedArray[i] < firstNumber) {
+            return false;
+        }
+    }
+    free(unsortedArray);
+    return true;
+
+}
+int main(void) {
+    if (!testCorrectCase()) {   
+        printf("Tests failed\n");
+        return;
+    }
+    int arrayLength = 20;
+    int *unsortedArray = malloc(arrayLength * sizeof(int));
+
+    fillingArrayOfRandowNumbers(unsortedArray, arrayLength);
+
+    printf("Array before:\n");
+    arrayOutput(unsortedArray, arrayLength);
+
+    halfOfQSort(unsortedArray, arrayLength);
+
+    printf("Array after: \n");
+    arrayOutput(unsortedArray, arrayLength);
+
+    free(unsortedArray);
+}
