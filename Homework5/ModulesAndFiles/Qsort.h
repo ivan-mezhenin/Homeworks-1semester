06bd3f9d7252f9f88db14a905d8968def7874962
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-10-23 22:10:04 +0300

    Full realization

--- Homework5/ModulesAndFiles/Qsort.h
+++ Homework5/ModulesAndFiles/Qsort.h
@@ -1,9 +1,13 @@
 #pragma once
 
+//сортировка сортировками, принимает массив, и границы, в пределах которых его нужно отсортировать
 void insertionSort(int array[], int left, int right);
 
+//быстрая сортировка, принимает массив, и границы, в пределах которых его нужно отсортировать
 void qSort(int array[], int left, int right);
 
+//функция для чтения массива и его длинны из файла, принимает имя файла, указатель на массив и его длинну
 void fileReadData(const char fileName[], int array[], int* arrayLength);
 
+//функция меняет местами два элемента, принимает два элемента
 void swapElements(int* left, int* right);
--- Homework5/ModulesAndFiles/main.c
+++ Homework5/ModulesAndFiles/main.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "Qsort.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
