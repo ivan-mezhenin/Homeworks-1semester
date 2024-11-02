52abc80f8d464f74689c5ba0e1896df24138115f
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-11-02 23:50:09 +0300

    refactor: sorts are checkes for the same arrays, delite variable index

--- Homework2/BubbleSortAndCountingSorting/BubbleSortAndCountingSorting.c
+++ Homework2/BubbleSortAndCountingSorting/BubbleSortAndCountingSorting.c
@@ -49,8 +49,7 @@ void countingSort(int* array, int length) {
     int* indexArray = calloc(maxOfarray + 1, sizeof(int));
 
     for (int i = 0; i < length; ++i) {
-        const int index = array[i];
-        indexArray[index]++;
+        indexArray[array[i]]++;
     }
     int counter = 0;
     for (int i = 0; i <= maxOfarray; ++i) {
@@ -88,7 +87,6 @@ bool testCorrectCaseBubbleSort() {
     }
     free(unsortedArray);
     return true;
-
 }
 
 int main(void) {
@@ -101,33 +99,38 @@ int main(void) {
     printf("Inter the length of array: ");
     scanf("%d", &length);
     printf("\n");
-    int* unsortedArray = malloc(length * sizeof(int));
-    fillingArrayOfRandowNumbers(unsortedArray, length);
+
+    int* unsortedArray1 = malloc(length * sizeof(int));
+    int* unsortedArray2 = malloc(length * sizeof(int));
+
+    fillingArrayOfRandowNumbers(unsortedArray1, length);
+    for (int i = 0; i < length; ++i) {
+        unsortedArray2[i] = unsortedArray1[i];
+    }
 
     if (length < 50) {
-        printf("Array Before sorting:\n");
-        arrayOutput(unsortedArray, length);
+        printf("Array before sorting:\n");
+        arrayOutput(unsortedArray1, length);
     }
     printf("\n");
 
     clock_t timeStart = clock();
-    countingSort(unsortedArray, length);
+    countingSort(unsortedArray1, length);
     clock_t timeCountingSort = clock() - timeStart;
 
     if (length < 50) {
-        printf("Array Before sorting:\n");
-        arrayOutput(unsortedArray, length);
+        printf("Array after sorting:\n");
+        arrayOutput(unsortedArray1, length);
     }
     printf("\n");
 
-    fillingArrayOfRandowNumbers(unsortedArray, length);
     clock_t timeStartBubbleSort = clock();
-    bubbleSort(unsortedArray, length);
+    bubbleSort(unsortedArray2, length);
     clock_t timeBubbleSort = clock() - timeStartBubbleSort;
 
     printf("Time of bubble sorting: %d\n", timeBubbleSort);
     printf("Time of counting sorting: %d\n", timeCountingSort);
 
-    free(unsortedArray);
-
+    free(unsortedArray1);
+    free(unsortedArray2);
 }
