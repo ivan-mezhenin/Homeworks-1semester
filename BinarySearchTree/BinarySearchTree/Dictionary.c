b374d3405b3979177ae0972ca6ddd82bba889d29
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-11-25 16:57:53 +0300

    feat: add some functions in module Dictionary

--- BinarySearchTree/BinarySearchTree/Dictionary.c
+++ BinarySearchTree/BinarySearchTree/Dictionary.c
@@ -1,12 +1,16 @@
 #include "Dictionary.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
+#define VALUE_SIZE 100
+
 typedef struct Node {
     int key;
     char* value;
+    struct Node* parent;
     struct Node* leftChild;
     struct Node* rightChild;
 } Node;
@@ -26,8 +30,41 @@ Dictionary* createSearchTree(int* errorCode) {
     return tree;
 }
 
+typedef enum {
+    left,
+    right
+} Position;
+
 bool isTreeEmpty(Dictionary* tree) {
-    return tree->root = NULL;
+    return tree->root == NULL;
+}
+
+Node* createNode(Node* parent, const int key, const char* value, int* errorCode) {
+    Node* newNode = calloc(1, sizeof(Node));
+    if (newNode == NULL) {
+        free(newNode);
+        *errorCode = MEMORY_ALLOCATION_ERROR;
+        return NULL;
+    }
+
+    newNode->parent = parent;
+    newNode->key = key;
+    newNode->value = calloc(sizeof(value), sizeof(char));
+    strcpy(newNode->value, value);
+    return newNode;
+}
+
+void addChild(Node* parent, const int key, const char* value, Position position, int* errorCode) {
+    Node* newNode = createNode(parent, key, value, errorCode);
+    if (*errorCode != 0) {
+        return;
+    }
+
+    if (position == left) {
+        parent->leftChild = newNode;
+        return;
+    }
+    parent->rightChild = newNode;
 }
 
 void insertValueInDictionary(Node* node, const int key, const char* value, int* errorCode) {
@@ -43,35 +80,19 @@ void insertValueInDictionary(Node* node, const int key, const char* value, int* 
 
     if (key < node->key) {
         if (node->leftChild == NULL) {
-            Node* newLeftChild = calloc(1, sizeof(Node));
-            if (newLeftChild == NULL) {
-                *errorCode = MEMORY_ALLOCATION_ERROR;
-                return;
-            }
-
-            newLeftChild->key = key;
-            strcpy(newLeftChild->value, value);
-            node->leftChild = newLeftChild;
+            addChild(node, key, value, left, errorCode);
             return;
         }
 
-        insertValueInTree(node->leftChild, key, value, errorCode);
+        insertValueInDictionary(node->leftChild, key, value, errorCode);
     }
     if (key > node->key) {
         if (node->rightChild == NULL) {
-            Node* newRightChild = calloc(1, sizeof(Node));
-            if (newRightChild == NULL) {
-                *errorCode = MEMORY_ALLOCATION_ERROR;
-                return;
-            }
-
-            newRightChild->key = key;
-            strcpy(newRightChild->value, value);
-            node->rightChild = newRightChild;
+            addChild(node, key, value, right, errorCode);
             return;
         }
 
-        insertValueInTree(node->rightChild, key, value, errorCode);
+        insertValueInDictionary(node->rightChild, key, value, errorCode);
     }
 }
 
@@ -87,11 +108,82 @@ void addValueInDictionary(Dictionary* tree, const int key, const char* value, in
             *errorCode = MEMORY_ALLOCATION_ERROR;
             return;
         }
+
         newRoot->key = key;
-        strcpy(newRoot->value, value);
+        strcpy_s(newRoot->value, VALUE_SIZE, value);
+
         tree->root = newRoot;
         return;
     }
 
     insertValueInDictionary(tree->root, key, value, errorCode);
+}
+
+char* get(Node* node, const int key, int* errorCode) {
+    if (node == NULL) {
+        *errorCode = POINTER_IS_NULL;
+        return NULL;
+    }
+
+    if (node->key == key) {
+        return node->value;
+    }
+
+    if (node->key > key) {
+        return get(node->leftChild, key, errorCode);
+    }
+
+    return get(node->rightChild, key, errorCode);
+}
+
+char* getValue(Dictionary* tree, const int key, int* errorCode) {
+    if (tree == NULL) {
+        *errorCode = POINTER_IS_NULL;
+        return NULL;
+    }
+
+    return get(tree->root, key, errorCode);
+}
+
+bool findKeyInDictionary(Node* node, const int key, int* errorCode) {
+    if (node == NULL) {
+        return false;
+    }
+    if (node->key == key) {
+        return true;
+    }
+    if (key > node->key) {
+        return findKeyInDictionary(node->rightChild, key, errorCode);
+    }
+    return findKeyInDictionary(node->leftChild, key, errorCode);
+}
+
+bool isKeyInDictionary(Dictionary* tree, const int key, int* errorCode) {
+    if (tree == NULL) {
+        *errorCode = POINTER_IS_NULL;
+        return false;
+    }
+
+    return findKeyInDictionary(tree->root, key, errorCode);
+}
+
+void inorderTraversal(Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->leftChild);
+        printf("%d: %s  ", root->key, root->value);
+        inorderTraversal(root->rightChild);
+    }
+}
+
+int main(void) {
+    int errorCode = 0;
+    Dictionary * tree = createSearchTree(&errorCode);
+    addValueInDictionary(tree, 5, "aboba", &errorCode);
+    addValueInDictionary(tree, 37, "pipipi", &errorCode);
+    addValueInDictionary(tree, 44, "bambamba", &errorCode);
+    addValueInDictionary(tree, 8, "tp24", &errorCode);
+    addValueInDictionary(tree, 1, "andrey", &errorCode);
+    addValueInDictionary(tree, 17, "vanya", &errorCode);
+    inorderTraversal(tree->root);
+
 }
--- BinarySearchTree/BinarySearchTree/Dictionary.h
+++ BinarySearchTree/BinarySearchTree/Dictionary.h
@@ -1,4 +1,32 @@
 #pragma once
 
+#include <stdbool.h>
+
 #define MEMORY_ALLOCATION_ERROR -1
 #define POINTER_IS_NULL -2
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct Node Node;
+
+typedef struct Dictionary Dictionary;
+
+Dictionary* createSearchTree(int* errorCode);
+
+typedef enum Position Position;
+
+bool isTreeEmpty(Dictionary* tree);
+
+Node* createNode(Node* parent, const int key, const char* value, int* errorCode);
+
+void addChild(Node* parent, const int key, const char* value, Position position, int* errorCode);
+
+void insertValueInDictionary(Node* node, const int key, const char* value, int* errorCode);
+void addValueInDictionary(Dictionary* tree, const int key, const char* value, int* errorCode);
+
+char* get(Node* node, const int key, int* errorCode);
+
+char* getValue(Dictionary* tree, const int key, int* errorCode);
+
+bool findKeyInDictionary(Node* node, const int key, int* errorCode);
+
+bool isKeyInDictionary(Dictionary* tree, const int key, int* errorCode);
