030ae095f28947eb8945441765aa6c25a414dc82
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-11-28 23:36:53 +0300

    build: full realization of module SyntaxTree

---
+++ SyntaxTree/SyntaxTree/SyntaxTree.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define MEMORY_ALLOCATION_ERROR -14141
+
+typedef struct SyntaxTree SyntaxTree;
+
+//build syntax tree
+void buildSyntaxTree(SyntaxTree** root, FILE* file, int* errorCode);
+
+//return result of calculation syntax tree
+int calculateSyntaxTree(SyntaxTree* root);
+
+//print syntax tree
+void printSyntaxTree(SyntaxTree* root);
+
+//delete syntax tree
+void deleteSyntaxTree(SyntaxTree** root);
