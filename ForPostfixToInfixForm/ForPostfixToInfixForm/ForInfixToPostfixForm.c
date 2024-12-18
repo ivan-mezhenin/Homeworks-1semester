17e222c3109ae93ffff11c8779c4a5f6dff2eb0f
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2024-12-18 23:53:20 +0300

    test: add tests

--- ForPostfixToInfixForm/ForPostfixToInfixForm/ForInfixToPostfixForm.c
+++ ForPostfixToInfixForm/ForPostfixToInfixForm/ForInfixToPostfixForm.c
@@ -1,14 +1,18 @@
 #include "/CHomeworks/Stack/charStack/charStack.h"
+#include "ForPostfixToInfixFormTest.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-#define _CRT_SECURE_NO_WARNINGS
 #define MAX_STRING_SIZE 101
-#define INCORRECT_EXPRESSION -1
-#define TESTS_FAILED -2
+#define INCORRECT_EXPRESSION -3
+#define TESTS_FAILED -4
+#define INVALID_OPERATION_WITH_EMPTY_STACK 1
+#define MEMORY_ALLOCATION_ERROR -1
+#define POINTER_IS_NULL -2
+#define NO_ERROR 0
 
 void printExpressionWithSpaces(char expression[], int length) {
     for (int i = 0; i < length; ++i) {
@@ -66,7 +70,7 @@ void convertExpressionFromInfixFormToPostfix(char infixExpression[], int stringL
 
         }
         else if (isOperation(currentSymbol)) {
-            while (topCharStack(signs, errorCode) != '\0' &&
+            while (getStackLength(signs) != 0 &&
                 (getOperationPrecedence(topCharStack(signs, errorCode)) >= getOperationPrecedence(currentSymbol))) {
                 postfixExpression[postfixExpressionIndex++] = popChar(signs, errorCode);
                 if (*errorCode != 0) {
@@ -81,7 +85,7 @@ void convertExpressionFromInfixFormToPostfix(char infixExpression[], int stringL
             }
         }
         else if (currentSymbol == ')') {
-            while (topCharStack(signs, errorCode) != '\0' && topCharStack(signs, errorCode) != '(') {
+            while (getStackLength(signs) != 0 && topCharStack(signs, errorCode) != '(') {
                 postfixExpression[postfixExpressionIndex++] = popChar(signs, errorCode);
                 if (*errorCode != 0) {
                     deleteCharStack(signs);
@@ -101,7 +105,7 @@ void convertExpressionFromInfixFormToPostfix(char infixExpression[], int stringL
         }
     }
 
-    while (topCharStack(signs, errorCode) != '\0') {
+    while (getStackLength(signs) != 0) {
         postfixExpression[postfixExpressionIndex++] = popChar(signs, errorCode);
         if (*errorCode != 0) {
             deleteCharStack(signs);
@@ -112,47 +116,13 @@ void convertExpressionFromInfixFormToPostfix(char infixExpression[], int stringL
     deleteCharStack(signs);
 }
 
-bool convertExpressionFromInfixFormToPostfixTest(void) {
-    const char* testInfix1 = "(1 + 1) * 2";
-    char postfix1[MAX_STRING_SIZE] = "";
-    int errorCode1 = 0;
-    const char* expectedResult1 = "11+2*";
-    convertExpressionFromInfixFormToPostfix(testInfix1, strlen(testInfix1), postfix1, &errorCode1);
-    if (strcmp(postfix1, expectedResult1) != 0) {
-        printf("Test (%s) failed\n", testInfix1);
-        return false;
-    }
-
-    const char* testInfix2 = "(2 + 3) * (4 + 5)";
-    char postfix2[MAX_STRING_SIZE] = "";
-    int errorCode2 = 0;
-    const char* expectedResult2 = "23+45+*";
-    convertExpressionFromInfixFormToPostfix(testInfix2, strlen(testInfix2), postfix2, &errorCode2);
-    if (strcmp(postfix2, expectedResult2) != 0) {
-        printf("Test (%s) failed\n", testInfix2);
-        return false;
-    }
-
-    const char* testInfix3 = "(1 + 6) * (5 + 3 * (7 + 8))";
-    char postfix3[MAX_STRING_SIZE] = "";
-    int errorCode3 = 0;
-    const char* expectedResult3 = "16+5378+*+*";
-    convertExpressionFromInfixFormToPostfix(testInfix3, strlen(testInfix3), postfix3, &errorCode3);
-    if (strcmp(postfix3, expectedResult3) != 0) {
-        printf("Test (%s) failed\n", testInfix3);
-        return false;
-    }
-
-    return true;
-}
-
 int main(void) {
     
-    if (!convertExpressionFromInfixFormToPostfixTest()) {
+    if (!test()) {
         return TESTS_FAILED;
     }
 
-    int errorCode = 0;
+    int errorCode = NO_ERROR;
     char infixExpression[MAX_STRING_SIZE] = "";
     char postfixExpression[MAX_STRING_SIZE] = "";
 
@@ -161,13 +131,21 @@ int main(void) {
 
     convertExpressionFromInfixFormToPostfix(infixExpression, strlen(infixExpression), postfixExpression, &errorCode);
 
-    if (errorCode == 0) {
+    if (errorCode == NO_ERROR) {
         printExpressionWithSpaces(postfixExpression, strlen(postfixExpression));
     }
     else if (errorCode == INCORRECT_EXPRESSION) {
         printf("Your expression is incorrect\n");
-        return INCORRECT_EXPRESSION;
+    }
+    else if (errorCode == POINTER_IS_NULL) {
+        printf("Passing a null pointer\n");
+    }
+    else if (errorCode == MEMORY_ALLOCATION_ERROR) {
+        printf("Memory allocation error\n");
+    }
+    else if (errorCode == INVALID_OPERATION_WITH_EMPTY_STACK) {
+        printf("Invalid operation with empty stack\n");
     }
     
-    return 0;
+    return errorCode;
 }
